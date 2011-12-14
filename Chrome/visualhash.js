0d5d134dee0eed892b7bd311d32145a8db7eda65
Paul Sawaya <me@paulsawaya.com>
2011-12-14 18:39:39 -0500

    still need to attach listener on page load

--- Chrome/visualhash.js
+++ Chrome/visualhash.js
@@ -1,11 +1,15 @@
 (function () {
-	document.addEventListener('DOMSubtreeModified', function(e) {
+	
+	function attachToInputs(e) {
 		var elements = document.getElementsByTagName('input');
 		for (var elIdx = 0; elIdx < elements.length; elIdx++) {
 	        if (elements[elIdx] && elements[elIdx].tagName == 'INPUT' && elements[elIdx].type == 'password' && !(elements[elIdx].__visualHash))
 	            attachHashAsYouType(elements[elIdx]);
 		}
-	});
+	}	
+	document.addEventListener('DOMSubtreeModified', attachToInputs);
+	
+	attachToInputs();
 	
     function attachHashAsYouType(passwordElem) {
         var oldBackgroundImage = passwordElem.style['backgroundImage'];
@@ -23,28 +27,24 @@
                 return;
             }
             var passwordHash = SHA1(elem.value);
-            // var gradientString = gradientStringForHash(passwordHash);
-
-            // elem.style['backgroundImage'] = gradientString;
             var elemWidth = Math.max(elem.clientWidth,elem.offsetWidth);
             var elemHeight = Math.max(elem.clientHeight,elem.offsetHeight);
             elem.style['backgroundImage'] = 'url(' + getDataURLForHash(passwordHash,elemWidth,elemHeight) + ')';
-            // elem.style['backgroundSize'] = '100%';
         }
         
         // TODO: find if there's another way of keeping track of typed updates.
         
-        function firstUpdate() {
+        function onKeyDown() {
             setTimeout(function() {
                 updateVisualHash(passwordElem)
             },10);
         }
-        passwordElem.addEventListener('keydown', firstUpdate);
+        passwordElem.addEventListener('keydown', onKeyDown);
         
         // FIXME: Find a better way of marking an element as having visual hashing attached.
         passwordElem.__visualHash = true;
         
         // Update for first keydown
-        firstUpdate();
+        onKeyDown();
     }
 })();
